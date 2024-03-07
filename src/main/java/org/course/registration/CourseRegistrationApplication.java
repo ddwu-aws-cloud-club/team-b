823e883f4bce69f9e8860f65cda93c80746f3b2c
yu-heejin <jkgt0521@naver.com>
2024-03-08 03:39:21 +0900

    feat: Spring Boot 기본 설정 (#1)

---
+++ src/main/java/org/course/registration/CourseRegistrationApplication.java
@@ -0,0 +1,12 @@
+package org.course.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegistrationApplication.class, args);
+    }
+}
