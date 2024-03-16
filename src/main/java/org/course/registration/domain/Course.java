53e3096934bfb66fe6cfb884d86b9f09c728ac51
ryu-hyesu <aimee3994@naver.com>
2024-03-16 20:39:28 +0900

    docs: course 주석 추가

--- src/main/java/org/course/registration/domain/Course.java
+++ src/main/java/org/course/registration/domain/Course.java
@@ -16,18 +16,19 @@ public class Course {
     @Id
     @GeneratedValue
     @Column(name = "course_id")
-    private int id;
+    private int id; // 과목 id
 
-    private String name;
+    private String name; // 과목 이름
 
-    private String pro;
+    private String pro; // 교수님 성함
 
-    private int limited;
+    private int limited; // 수강 정원
 
-    private int count;
+    private int count; // 현재 정원
 
     //== 비지니스 로직==//
     // 과목 엔티티는 count를 속성으로 가지고 있기 때문에 변경 시 setter getter를 통하는 것이 아니라 내부에 관련 비지니스 로직을 넣는 것이 좋다고 해서 넣었습니다
+
     /**
      * 신청 인원 증가
      */
