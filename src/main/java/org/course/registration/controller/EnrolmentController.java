e97af725c9626ad7ddfc2f53dde93e874486d175
ryu-hyesu <aimee3994@naver.com>
2024-03-16 23:59:56 +0900

    add: 과목 조회

--- src/main/java/org/course/registration/controller/EnrolmentController.java
+++ src/main/java/org/course/registration/controller/EnrolmentController.java
@@ -1,15 +1,27 @@
 package org.course.registration.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.course.registration.domain.Course;
+import org.course.registration.service.CourseService;
+import org.course.registration.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-@Slf4j
+@RequiredArgsConstructor
 public class EnrolmentController {
 
+    private final CourseService courseService;
+
     @GetMapping("/enrolment")
-    public String enrolment(){
+    public String enrolment(Model model){
+        List<Course> courses = courseService.findCourses();
+        model.addAttribute("courses", courses);
+
         return "enrolls";
     }
 
