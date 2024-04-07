8a439a2db4fa1c0d5379ada7034356f9f69b729b
yu-heejin <jkgt0521@naver.com>
2024-04-07 14:37:46 +0900

    hotfix: 실행 스크립트에 서브모듈 업데이트 추가

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ..
+git submodule update --remote --recursive --init
 ./gradlew clean build
 java -Djarmode=layertools -jar build/libs/class-registration-1.0-SNAPSHOT.jar extract
 docker-compose up -d --build
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ..
+git submodule update --remote --recursive --init
 ./gradlew clean build
 java -Djarmode=layertools -jar build/libs/class-registration-1.0-SNAPSHOT.jar extract
 docker build -f Dockerfile.prod -t team-b-server-prod .
